338ae335b3fffee6e42047ddd3687b06296e2819
IlikeBB <32098079+IlikeBB@users.noreply.github.com>
2022-10-05 15:48:35 +0800

    Add files via upload

--- utils/loss.py
+++ utils/loss.py
@@ -1,21 +1,39 @@
 #PyTorch
 import torch.nn.functional as F
-
 import torch.nn as nn
 import torch
 # loss function
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+        
+        return Dice_BCE
+
 class FocalTverskyLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
-        self.alpha = 0.5
-        self.beta = 0.5
-        self.gamma = 1.00
+        self.alpha = 0.3
+        self.beta = 0.7
+        self.gamma = 2
         self.smooth = 1e-6
         super(FocalTverskyLoss, self).__init__()
 
     def forward(self, inputs, targets):
         #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = torch.sigmoid(inputs)
-        targets  = F.one_hot(targets, num_classes=2)
+        inputs = inputs.sigmoid()
+
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
         targets = targets.view(-1)
@@ -46,27 +64,6 @@ class FocalLoss(nn.Module):
         focal_loss = alpha * (1-BCE_EXP)**gamma * BCE
                        
         return focal_loss
-
-
-class DiceBCELoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
-        super(DiceBCELoss, self).__init__()
-
-    def forward(self, inputs, targets, smooth=1):
-        #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = torch.sigmoid(inputs)       
-        
-        #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-        
-        intersection = (inputs * targets).sum()                            
-        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
-        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
-        Dice_BCE = BCE + dice_loss
-        
-        return Dice_BCE
-
 
 # metric
 def dice(inputs, targets):
